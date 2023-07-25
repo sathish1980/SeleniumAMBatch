5e90842166c83afd0e437b4958d525d4a04e49f7
sathish1980 <kumar.sathish189@gmail.com>
2023-07-25 09:04:15 +0530

    second class

--- src/test/java/SeleniumBasics/FirstClass.java
+++ src/test/java/SeleniumBasics/FirstClass.java
@@ -1,6 +1,8 @@
 package SeleniumBasics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstClass {
@@ -10,7 +12,15 @@ public class FirstClass {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys("sathish");
+		username.clear();
+		driver.findElement(By.id("email")).sendKeys("Kumar");
+		driver.findElement(By.name("email")).sendKeys("besant");
 		
+		//driver.findElement(By.className("inputtext _55r1 _6luy")).sendKeys("hi");
+		//driver.findElement(By.linkText("Forgotten password?")).click();
+		driver.findElement(By.partialLinkText("pass")).click();
 		//driver.close();
 		//driver.quit();
 	}
